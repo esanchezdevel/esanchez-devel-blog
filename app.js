24c620f47065ec76cc3866ad6df755d197afb88f
Enrique Sanchez <enrique.sanchez@hpe.com>
2023-11-16 16:15:49 +0100

    feat: use mustache as views engine to be able to use templates

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
+const path = require('path');
 const app = express();
 
 const port = 4200;
 
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index', { title: 'esanchez devel'});
 });
 
 app.listen(port, () => {
