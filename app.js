e405367bd32e7b39130b172308985373263194df
Enrique Sanchez <enrique.sanchez@hpe.com>
2023-11-16 16:32:30 +0100

    feat: use css styles in index

--- app.js
+++ app.js
@@ -9,8 +9,14 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'views'));
 
+// configure express to serve files in "public" folder like .css or .js files
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.render('index', { title: 'esanchez devel'});
+    res.render('index', { 
+        title: 'esanchez devel', 
+        description: 'Programador experto en Java con una gran afici&oacute;n por seguir aprendiendo cosas nuevas.'
+    });
 });
 
 app.listen(port, () => {
