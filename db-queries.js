eca1ea10b43f35a5927156a217954f763932a8c8
Enrique Sanchez <enrique.sanchez@hpe.com>
2023-11-20 18:31:11 +0100

    feat: query to insert site configuration

---
+++ db-queries.js
@@ -0,0 +1,7 @@
+
+// Add site configurations
+db.siteConfigurations.insert([
+    {"title": "Aprende con esanchez"}, 
+    {"description": "Programador experto en Java con una gran afici&oacute;n por seguir aprendiendo cosas nuevas."}
+])
+
