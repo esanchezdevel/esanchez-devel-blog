af9c36d97acb27c499c2e9ebf440e6d83961245a
Enrique Sanchez <esanchez.devel@gmail.com>
2024-01-05 13:55:18 +0100

    feat: modify deploy local container script

--- deploy_local_container.sh
+++ deploy_local_container.sh
@@ -3,18 +3,18 @@
 echo ""
 echo "Clean local environment"
 echo ""
-docker container stop elrincondeesanchez
-docker container rm elrincondeesanchez
+docker container stop elrincondekike
+docker container rm elrincondekike
 
-docker image rm esanchez-devel-blog/esanchez-devel-blog-app:latest
+docker image rm esanchezdevel/esanchez-devel-blog:latest
 
 echo ""
 echo "Build image..."
 echo ""
-docker build -t esanchez-devel-blog/esanchez-devel-blog-app .
+docker build -t esanchezdevel/esanchez-devel-blog .
 
 echo ""
 echo "Run container..."
-docker run --name elrincondeesanchez -p4200:4200 --network esanchez-devel-blog -d esanchez-devel-blog/esanchez-devel-blog-app:latest
+docker run --name elrincondekike -p4200:4200 --network esanchez-devel-blog -d esanchezdevel/esanchez-devel-blog:latest
 echo ""
 echo "Done!!!"
